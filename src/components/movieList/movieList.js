9ee16ce5bcb63dece560b4253899a5bfc064f6d9
joshi-tejas07 <103984216+joshi-tejas07@users.noreply.github.com>
2024-05-22 15:31:16 +0530

    update

--- src/components/movieList/movieList.js
+++ src/components/movieList/movieList.js
@@ -5,16 +5,27 @@ import Cards from "../card/card";
 
 const MovieList = () => {
     const [movieList, setMovieList] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
     const { type } = useParams();
 
     useEffect(() => {
-        getData();
-    }, [type]);
+        getData(currentPage);
+    }, [type, currentPage]);
 
-    const getData = () => {
-        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=c45a857c193f6302f2b5061c3b85e743&language=en-US`)
+    const getData = (page) => {
+        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=c45a857c193f6302f2b5061c3b85e743&language=en-US&page=${page}`)
             .then(res => res.json())
-            .then(data => setMovieList(data.results));
+            .then(data => {
+                setMovieList(data.results);
+                setTotalPages(data.total_pages);
+            });
+    };
+
+    const handlePageChange = (newPage) => {
+        if (newPage > 0 && newPage <= totalPages) {
+            setCurrentPage(newPage);
+        }
     };
 
     return (
@@ -24,6 +35,15 @@ const MovieList = () => {
                 {movieList.map(movie => (
                     <Cards key={movie.id} movie={movie} />
                 ))}
+            </div>
+            <div className="pagination">
+                <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
+                    Previous
+                </button>
+                <span>Page {currentPage} of {totalPages}</span>
+                <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>
+                    Next
+                </button>
             </div>
         </div>
     );
--- src/pages/search/SearchResults.js
+++ src/pages/search/SearchResults.js
@@ -5,25 +5,34 @@ import { useLocation } from 'react-router-dom';
 
 const SearchResults = () => {
   const [movies, setMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const query = queryParams.get('query') || '';
 
   useEffect(() => {
     if (query) {
-      handleSearch();
+      handleSearch(currentPage);
     }
-  }, [query]);
+  }, [query, currentPage]);
 
-  const handleSearch = async () => {
+  const handleSearch = async (page) => {
     try {
       const response = await fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=c45a857c193f6302f2b5061c3b85e743&language=en-US&query=${query}`
+        `https://api.themoviedb.org/3/search/movie?api_key=c45a857c193f6302f2b5061c3b85e743&language=en-US&query=${query}&page=${page}`
       );
       const data = await response.json();
       setMovies(data.results);
+      setTotalPages(data.total_pages);
     } catch (error) {
       console.error('Error fetching data:', error);
+    }
+  };
+
+  const handlePageChange = (newPage) => {
+    if (newPage > 0 && newPage <= totalPages) {
+      setCurrentPage(newPage);
     }
   };
 
@@ -34,6 +43,15 @@ const SearchResults = () => {
         {movies.map((movie) => (
           <Cards key={movie.id} movie={movie} />
         ))}
+      </div>
+      <div className="pagination">
+        <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <span>Page {currentPage} of {totalPages}</span>
+        <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>
+          Next
+        </button>
       </div>
     </div>
   );
